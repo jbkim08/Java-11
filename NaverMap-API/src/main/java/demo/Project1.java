4a17bd8a425b35d5081fb471f1a0c0b34494203f
jbkim <drv98@naver.com>
2022-09-29 15:35:59 +0900

    네이버맵2

--- NaverMap-API/src/main/java/demo/Project1.java
+++ NaverMap-API/src/main/java/demo/Project1.java
@@ -15,44 +15,50 @@ import org.json.simple.parser.ParseException;
 
 public class Project1 {
 	public static void main(String[] args) throws IOException, ParseException {
-		
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("주소를 입력: ");
 		String address = scanner.nextLine();
 		scanner.close();
-		
-		StringBuilder urlBuilder = new StringBuilder("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=");
-		urlBuilder.append(URLEncoder.encode(address,"UTF-8")); //한글 주소를 utf-8로 인코딩해서 요청
-		
-		URL url = new URL(urlBuilder.toString()); //URL 주소 객체 생성
-		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-		//연결설정
-		conn.setRequestMethod("GET"); //요청방법 "GET"
+
+		StringBuilder urlBuilder = new StringBuilder(
+				"https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=");
+		urlBuilder.append(URLEncoder.encode(address, "UTF-8")); // 한글 주소를 utf-8로 인코딩해서 요청
+
+		URL url = new URL(urlBuilder.toString()); // URL 주소 객체 생성
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		// 연결설정
+		conn.setRequestMethod("GET"); // 요청방법 "GET"
 		conn.setRequestProperty("Content-type", "application/json");
 		conn.setRequestProperty("X-NCP-APIGW-API-KEY-ID", "omcmy4brbg");
 		conn.setRequestProperty("X-NCP-APIGW-API-KEY", "fEzstawjhbrab8ch9c2LUCzY7MJoYrN0KDbcJ6gG");
-		
-        BufferedReader br;
-        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-        }
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = br.readLine()) != null) {
-            sb.append(line);
-        }
-        br.close();
-        conn.disconnect();
-        System.out.println(sb.toString());
-        JSONParser jsonParser = new JSONParser(); //제이슨 변환 객체
-        JSONObject jsonObject = (JSONObject)jsonParser.parse(sb.toString());
-        JSONArray arr = (JSONArray)jsonObject.get("data");
+
+		BufferedReader br;
+		if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+			br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		} else {
+			br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+		}
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = br.readLine()) != null) {
+			sb.append(line);
+		}
+		br.close();
+		conn.disconnect();
+		// System.out.println(sb.toString());
+		JSONParser jsonParser = new JSONParser(); // 제이슨 변환 객체
+		JSONObject jsonObject = (JSONObject) jsonParser.parse(sb.toString());
+		JSONArray arr = (JSONArray) jsonObject.get("addresses");
+
+		for (Object one : arr) {
+			JSONObject ob = (JSONObject) one;
+			// System.out.println(employee.toString());
+			System.out.println("address:" + ob.get("roadAddress"));
+			System.out.println("jibunAddress:" + ob.get("jibunAddress"));
+			System.out.println("경도:" + ob.get("x"));
+			System.out.println("위도:" + ob.get("y"));
+		}
 	}
 
 }
-
-
-
-
