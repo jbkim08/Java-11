c4a486e24536d175e50bb8ef1e761e6afea36557
jbkim <drv98@naver.com>
2022-09-29 14:13:45 +0900

    공공데이터2

--- gson-test/src/main/java/demo/JsonSimple.java
+++ gson-test/src/main/java/demo/JsonSimple.java
@@ -5,11 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class JsonSimple {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParseException {
         StringBuilder urlBuilder = new StringBuilder("https://api.odcloud.kr/api/15077586/v1/centers"); /*Base주소*/
         urlBuilder.append("?serviceKey=" + "982NxEgdH6thK%2FZDz70UrCfhK7oAZbmNwMC5vj%2Ftc9p7MhU5F9xBeJD47SMMIMfpvUbADVmpJPpZzqFqZDVwPg%3D%3D"); /*Service Key*/
         urlBuilder.append("&page=1"); /*페이지번호*/
@@ -17,9 +21,9 @@ public class JsonSimple {
         
         URL url = new URL(urlBuilder.toString()); //주소로 URL객체 생성
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-        System.out.println("Response code: " + conn.getResponseCode());
+        conn.setRequestMethod("GET"); //요청메소드 get
+        conn.setRequestProperty("Content-type", "application/json");//데이터 타입 json
+        //System.out.println("Response code: " + conn.getResponseCode());
         BufferedReader rd;
         if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
             rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
@@ -33,7 +37,20 @@ public class JsonSimple {
         }
         rd.close();
         conn.disconnect();
-        System.out.println(sb.toString());
+        //System.out.println(sb.toString());
+        JSONParser jsonParser = new JSONParser(); //제이슨 변환 객체
+        JSONObject jsonObject = (JSONObject)jsonParser.parse(sb.toString());
+        JSONArray arr = (JSONArray)jsonObject.get("data");
+        //System.out.println(arr);
+        for(Object one : arr) {
+        	JSONObject ob = (JSONObject)one;
+        	System.out.print(ob.get("id") + "\t");
+        	System.out.print(ob.get("facilityName") + "\t");
+        	System.out.print(ob.get("address") + "\t");
+        	System.out.print(ob.get("org") + "\t");
+        	System.out.print(ob.get("createdAt") + "\t");
+        	System.out.println(ob.get("phoneNumber"));
+        }
 	}
 
 }
